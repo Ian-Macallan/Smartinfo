d8c6db6b29f1363ceea9b3f76fa92a1b85c23241
Macallan <ian.macallan@gmail.com>
2025-05-24 16:25:20 +0200

    Minor

--- AutomaticProductName.h
+++ AutomaticProductName.h
@@ -1,22 +1,27 @@
 #pragma once
 
+#ifndef AUTOMATICPRODUCTNAME_H
+#define AUTOMATICPRODUCTNAME_H    1
+
 #ifdef _WIN64
 #ifdef UNICODE
 #define ORIGINAL_FILENAME   L"SmartInfo (x64 Unicode) (MSVC)\0"
-#define PRODUCT_NAME        L"SmartInfo - Version 1.0.04.017\r\n(Build 70) - (x64 Unicode) (MSVC)\0"
+#define PRODUCT_NAME        L"SmartInfo - Version 1.0.05.002\r\n(Build 74) - (x64 Unicode) (MSVC)\0"
 #else
 #define ORIGINAL_FILENAME   "SmartInfo (x64 MBCS) (MSVC)\0"
-#define PRODUCT_NAME        "SmartInfo - Version 1.0.04.017\r\n(Build 70) - (x64 MBCS) (MSVC)\0"
+#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.002\r\n(Build 74) - (x64 MBCS) (MSVC)\0"
 #endif
 #elif _WIN32
 #ifdef UNICODE
 #define ORIGINAL_FILENAME   L"SmartInfo (x86 Unicode) (MSVC)\0"
-#define PRODUCT_NAME        L"SmartInfo - Version 1.0.04.017\r\n(Build 70) - (x86 Unicode) (MSVC)\0"
+#define PRODUCT_NAME        L"SmartInfo - Version 1.0.05.002\r\n(Build 74) - (x86 Unicode) (MSVC)\0"
 #else
 #define ORIGINAL_FILENAME   "SmartInfo (x86 MBCS) (MSVC)\0"
-#define PRODUCT_NAME        "SmartInfo - Version 1.0.04.017\r\n(Build 70) - (x86 MBCS) (MSVC)\0"
+#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.002\r\n(Build 74) - (x86 MBCS) (MSVC)\0"
 #endif
 #else
 #define ORIGINAL_FILENAME   "SmartInfo (MSVC)\0"
-#define PRODUCT_NAME        "SmartInfo - Version 1.0.04.017\r\n(Build 70) - (MSVC)\0"
+#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.002\r\n(Build 74) - (MSVC)\0"
+#endif
+
 #endif
--- ProductVersionInfo.cmd
+++ ProductVersionInfo.cmd
@@ -3,6 +3,6 @@
 rem cd %~dp0
 rem %~d0
 
-set PRODUCT_VERSION_VERS=1.0.4.17
+set PRODUCT_VERSION_VERS=1.0.5.2
 
 @exit /b

a2ace91f94930f55612245327d06959d3f5543cd
Macallan <ian.macallan@gmail.com>
2026-08-23 12:53:22 +0200

    Platform

--- AutomaticProductName.h
+++ AutomaticProductName.h
@@ -6,22 +6,22 @@
 #ifdef _WIN64
 #ifdef UNICODE
 #define ORIGINAL_FILENAME   L"SmartInfo (x64 Unicode) (MSVC)\0"
-#define PRODUCT_NAME        L"SmartInfo - Version 1.0.05.005\r\n(Build 77) - (x64 Unicode) (MSVC)\0"
+#define PRODUCT_NAME        L"SmartInfo - Version 1.0.05.006\r\n(Build 78) - (x64 Unicode) (MSVC)\0"
 #else
 #define ORIGINAL_FILENAME   "SmartInfo (x64 MBCS) (MSVC)\0"
-#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.005\r\n(Build 77) - (x64 MBCS) (MSVC)\0"
+#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.006\r\n(Build 78) - (x64 MBCS) (MSVC)\0"
 #endif
 #elif _WIN32
 #ifdef UNICODE
 #define ORIGINAL_FILENAME   L"SmartInfo (x86 Unicode) (MSVC)\0"
-#define PRODUCT_NAME        L"SmartInfo - Version 1.0.05.005\r\n(Build 77) - (x86 Unicode) (MSVC)\0"
+#define PRODUCT_NAME        L"SmartInfo - Version 1.0.05.006\r\n(Build 78) - (x86 Unicode) (MSVC)\0"
 #else
 #define ORIGINAL_FILENAME   "SmartInfo (x86 MBCS) (MSVC)\0"
-#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.005\r\n(Build 77) - (x86 MBCS) (MSVC)\0"
+#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.006\r\n(Build 78) - (x86 MBCS) (MSVC)\0"
 #endif
 #else
 #define ORIGINAL_FILENAME   "SmartInfo (MSVC)\0"
-#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.005\r\n(Build 77) - (MSVC)\0"
+#define PRODUCT_NAME        "SmartInfo - Version 1.0.05.006\r\n(Build 78) - (MSVC)\0"
 #endif
 
 #endif
--- AutomaticVersionHeader.h
+++ AutomaticVersionHeader.h
@@ -5,7 +5,7 @@
 //
 //	(c) 2003 - 2026 Macallan - mailto:ian.macallan@gmail.com
 //
-//	Generated : 28/05/2026 13:14:17
+//	Generated : 23/08/2026 12:48:45
 //
 //===========================================================================================
 //
@@ -57,22 +57,22 @@
 #endif
 
 #ifdef UNICODE
-#define PROGRAM_DATE    L"Thu May 28 13:14"
-#define PROGRAM_DATE_F  L"Thu May 28 13:14 2026"
+#define PROGRAM_DATE    L"Sun Aug 23 12:48"
+#define PROGRAM_DATE_F  L"Sun Aug 23 12:48 2026"
 #define PROGRAM_YEAR    L"2026"
-#define PROGRAM_TIME    L"2026/05/28 13:14:17"
+#define PROGRAM_TIME    L"2026/08/23 12:48:45"
 #else
-#define PROGRAM_DATE    "Thu May 28 13:14"
-#define PROGRAM_DATE_F  "Thu May 28 13:14 2026"
+#define PROGRAM_DATE    "Sun Aug 23 12:48"
+#define PROGRAM_DATE_F  "Sun Aug 23 12:48 2026"
 #define PROGRAM_YEAR    "2026"
-#define PROGRAM_TIME    "2026/05/28 13:14:17"
+#define PROGRAM_TIME    "2026/08/23 12:48:45"
 #endif
 
 #ifdef UNICODE
-#define PROGRAM_VERSION L"1.0.05.005"
-#define PROGRAM_INSTALL L"1.0.05005"
-#define PROGRAM_VER_NUM 10005005
-#define PROGRAM_BUILD   L"77"
+#define PROGRAM_VERSION L"1.0.05.006"
+#define PROGRAM_INSTALL L"1.0.05006"
+#define PROGRAM_VER_NUM 10005006
+#define PROGRAM_BUILD   L"78"
 #define PROGRAM_TITLE   L"Smart Info"
 #ifdef _WIN64
 #define PROGRAM_TITLE_P L"Smart Info (x64 Unicode)"
@@ -80,10 +80,10 @@
 #define PROGRAM_TITLE_P L"Smart Info (x86 Unicode)"
 #endif
 #else
-#define PROGRAM_VERSION "1.0.05.005"
-#define PROGRAM_INSTALL "1.0.05005"
-#define PROGRAM_VER_NUM 10005005
-#define PROGRAM_BUILD   "77"
+#define PROGRAM_VERSION "1.0.05.006"
+#define PROGRAM_INSTALL "1.0.05006"
+#define PROGRAM_VER_NUM 10005006
+#define PROGRAM_BUILD   "78"
 #define PROGRAM_TITLE   "Smart Info"
 #ifdef _WIN64
 #define PROGRAM_TITLE_P "Smart Info (x64 MBCS)"
